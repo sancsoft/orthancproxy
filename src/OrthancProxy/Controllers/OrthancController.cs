3a246663e26fd661361ecfcd5b6138500a6fc2cc
tgrubb <tgrubb@sancsoft.com>
2020-07-19 17:45:10 -0400

    Added the Web API to proxy GET orthanc requests to an orthanc server and return the responses.

---
+++ src/OrthancProxy/Controllers/OrthancController.cs
@@ -0,0 +1,79 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace OrthancProxy.Controllers
+{
+    [ApiController]
+    // {*path} allows us to capture all routes.
+    [Route("orthanc/{*path}")]
+    public class OrthancController : ControllerBase
+    {
+        private readonly ILogger<OrthancController> _logger;
+        
+        public OrthancController(ILogger<OrthancController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAsync(string path)
+        {
+            try 
+            {
+                // Get the base url to which we are acting as a proxy for.
+                var baseUrl = Request.Headers["BaseUrl"].FirstOrDefault();
+                if(baseUrl == null) 
+                {
+                    return BadRequest("BaseUrl header is required");
+                }
+
+                // Create the HttpClient to make our request.
+                HttpClient httpClient = new HttpClient();
+
+                // Copy the Basic Auth and add it to our request.
+                var auth = Request.Headers["Authorization"].FirstOrDefault();
+                if(auth != null) 
+                {
+                    httpClient.DefaultRequestHeaders.Add("Authorization", auth);
+                }
+                else 
+                {
+                    // Maybe the target endpoint is unsecured?
+                }
+
+                // Set the BaseAddress for our request.
+                httpClient.BaseAddress = new Uri(baseUrl);
+
+                // Add any query params to the path.
+                string pathAndQuery = path + Request.QueryString;
+
+                // Make the GET request.
+                var httpResponse = await httpClient.GetAsync(pathAndQuery);
+
+                // Forward the response.
+                switch(httpResponse.StatusCode) 
+                {
+                    case HttpStatusCode.OK:
+                        return Ok(await httpResponse.Content.ReadAsStringAsync());
+                    case HttpStatusCode.Unauthorized:
+                        return Unauthorized();
+                    case HttpStatusCode.NotFound:
+                        return NotFound();
+                    default:
+                        return BadRequest("Unhandled status code: " + (int)httpResponse.StatusCode);
+                }
+            }
+            catch(Exception ex) 
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ src/OrthancProxy/Startup.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace OrthancProxy
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            // Configure the CORS clients.
+            services.AddCors(o => o.AddPolicy("CorsPolicy", builder =>
+            {
+                // Annoying way to get the array of TrustedUrls from the config.
+                builder.WithOrigins(Configuration.GetSection("TrustedUrls").GetChildren().ToArray().Select(c => c.Value).ToArray())
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials();
+            }));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            // Use CORS.
+            app.UseCors("CorsPolicy");
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
